6d956e33c616ff801c6d9b7ce563f5833f1ff4a1
BlueCP <47088138+BlueCP@users.noreply.github.com>
2023-02-05 03:08:02 +0000

    Changes

--- app.py
+++ app.py
@@ -3,37 +3,28 @@ from flask import url_for
 from flask import render_template
 from flask import request
 from flask import redirect
-from flask import  Response, request
+from flask import Response
 from webcam import get_brandname
 import webcam
-import cv2 
-import get_report
+import cv2
+from get_report import get_report
 
 camera = cv2.VideoCapture(0)
 old_brand_name = ''
 
 app = Flask(__name__)
 
-def handle_request(product_name):
-    # Add more processing here in future.
-    return
-
 @app.route('/')
 def welcome():
     return redirect(url_for('scanning'))
 
-@app.route('/scanning')
-def scanning():
-    return render_template('result.html')
-
 @app.route('/processing')
 def processing():
-    input_json = request.json
+    # input_json = request.json
     if old_brand_name != get_brandname():
         old_brand_name = get_brandname()
         return get_report(old_brand_name)
     return
-
 
 @app.route('/video_feed')
 def video_feed():
